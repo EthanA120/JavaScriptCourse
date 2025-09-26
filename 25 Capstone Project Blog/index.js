ab9b2f357b64a56542a300f4e6504d732e7abb78
Ethan <EthanA120@Gmail.com>
2025-09-26 17:10:36 +0300

    Capstone Project - 2

--- 25 Capstone Project Blog/index.js
+++ 25 Capstone Project Blog/index.js
@@ -4,6 +4,10 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+// app.use(express.json()); // Enable JSON body parsing
+app.use(express.urlencoded({ extended: true })); // Enable URL-encoded body parsing
+
+
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
@@ -13,10 +17,11 @@ app.get("/posts", (req, res) => {
     res.render("posts.ejs");
 });
 
-app.get("/newPost", (req, res) => {
+app.post("/newPost", (req, res) => {
+    // console.log(req.body); // Access the parsed JSON data
     res.render("newPost.ejs");
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}.`);
+    console.log(`Server running on http://localhost:${port}.`);
 });
